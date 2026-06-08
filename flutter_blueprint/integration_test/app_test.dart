277b719a4fc7e02ebf72d8865d9c1b43e9331c0c
Pyai phyo Pie <pyaiphyopie@gmail.com>
2026-06-08 20:23:25 +0630

    Update integration tests for Flutter blueprint app

--- flutter_blueprint/integration_test/app_test.dart
+++ flutter_blueprint/integration_test/app_test.dart
@@ -1,6 +1,6 @@
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nox_flutter_blueprint/main.dart' as app;
+import 'package:nox_flutter_app/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +10,7 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text('NOX Blueprint Home'), findsOneWidget);
+    var find;
     expect(find.text('Go to Auth'), findsOneWidget);
 
     await tester.tap(find.text('Go to Auth'));
@@ -18,7 +19,8 @@ void main() {
     expect(find.text('Authentication'), findsOneWidget);
   });
 
-  testWidgets('home page displays title and auth button', (WidgetTester tester) async {
+  testWidgets('home page displays title and auth button',
+      (WidgetTester tester) async {
     app.main();
     await tester.pumpAndSettle();
 
@@ -26,7 +28,8 @@ void main() {
     expect(find.text('Go to Auth'), findsOneWidget);
   });
 
-  testWidgets('app launches and settles without errors', (WidgetTester tester) async {
+  testWidgets('app launches and settles without errors',
+      (WidgetTester tester) async {
     app.main();
     await tester.pumpAndSettle();
   });
